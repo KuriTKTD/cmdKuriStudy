f53be0cd486301b580c16eaf12ab0b2761d903d7
KuriTKTD <72000674+KuriTKTD@users.noreply.github.com>
2023-11-30 20:00:47 -0300

    Adding files via upload - Batch script study
    Script Batch  study

---
+++ scriptbatch.bat
@@ -0,0 +1,8 @@
+@Echo off
+cd "C:\Users\rodol\Desktop\2023 All Archives Kuri PC"
+FOR /F %%Z IN (*) DO (Echo %%Z)
+pause
+
+
+cd "C:\Users\rodol\Desktop\2023 All Archives Kuri PC"
+cd C:\Users\rodol\Desktop
